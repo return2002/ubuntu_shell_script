d9c34ed8318bee73297205cdb1f994ed33f78638
return2002 <59769007+return2002@users.noreply.github.com>
2024-07-26 10:36:29 +0800

    Update docker_compose_jupyter.sh

--- docker_compose_jupyter.sh
+++ docker_compose_jupyter.sh
@@ -6,11 +6,11 @@ mkdir -p /home/return2002
 # 进入目录
 cd /home/return2002
 
-sudo docker pull dataquestio/python3-starter
+sudo docker pull jupyter/base-notebook
 
 # 提示用户输入端口号
 read -p "请输入宿主端口号（格式为宿主机端口:容器端口）: " PORT
-sudo docker run --name jupyter -d -p $PORT:8888 -v /home/return2002/notebooks:/home/ds/notebooks dataquestio/python3-starter
+sudo docker run --name jupyter -d -p $PORT:8888 -v /home/return2002/notebooks:/home/ds/notebooks jupyter/base-notebook
 
 docker exec -it jupyter pip install ipywidgets
 
