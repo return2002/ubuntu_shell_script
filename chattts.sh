35f851b5c5f93a7fb24feb497198a24c0f0fe5dd
return2002 <59769007+return2002@users.noreply.github.com>
2025-03-04 23:11:29 +0800

    Update chattts.sh

--- chattts.sh
+++ chattts.sh
@@ -7,10 +7,10 @@ mkdir -p /home/return2002
 cd /home/return2002
 
 # 克隆 GitHub 仓库到 chat-tts-ui 目录
-git clone https://github.com/jianchang512/ChatTTS-ui.git chat-tts-ui
+git clone https://github.com/jianchang512/ChatTTS-ui.git app
 
-# 进入 chat-tts-ui 目录
-cd chat-tts-ui
+# 进入 app 目录
+cd app
 
 # 使用 docker-compose.cpu.yaml 文件启动容器
 docker compose -f docker-compose.cpu.yaml up -d
