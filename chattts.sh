f1e27dc792b47b302127d8cdfcef270cd5538eec
return2002 <59769007+return2002@users.noreply.github.com>
2025-03-04 22:57:10 +0800

    chattts

---
+++ chattts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 创建目录
+mkdir -p /home/return2002
+
+# 进入目录
+cd /home/return2002
+
+# 克隆 GitHub 仓库到 chat-tts-ui 目录
+git clone https://github.com/jianchang512/ChatTTS-ui.git chat-tts-ui
+
+# 进入 chat-tts-ui 目录
+cd chat-tts-ui
+
+# 使用 docker-compose.cpu.yaml 文件启动容器
+docker compose -f docker-compose.cpu.yaml up -d
